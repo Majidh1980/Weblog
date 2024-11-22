0196eece575be7aa994e987e7ef11fcc1fdcba74
Majid <majidh2008@yahoo.com>
2024-11-22 18:18:01 +0330

    some models and admin added

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from posts.models import Post
 
 
 def index(request):
@@ -8,3 +10,9 @@ def index(request):
 
 def home(request):
     return HttpResponse('<h3>Welcome to my blog... .</h3>')
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'posts/post_list.html', context=context)
